29fecb1ce1a4494f2b470abfe25f7d3b62864104
Alexander S <oleksandr.slv@gmail.com>
2022-05-04 15:32:32 +0300

    add logic and styles

--- src/Board.js
+++ src/Board.js
@@ -1,88 +1,78 @@
-import React, {Component} from "react";
-import Cell from "./Cell";
+import { useState } from 'react';
+import Cell from './Cell';
 import './Board.css';
 
+export default function Board({ nrows = 5, ncols = 5, chanceLightStartsOn = 0.25 }) {
+  const [hasWon, setHasWon] = useState(false);
+  const [board, setBoard] = useState(createBoard());
 
-/** Game board of Lights out.
- *
- * Properties:
- *
- * - nrows: number of rows of board
- * - ncols: number of cols of board
- * - chanceLightStartsOn: float, chance any cell is lit at start of game
- *
- * State:
- *
- * - hasWon: boolean, true when board is all off
- * - board: array-of-arrays of true/false
- *
- *    For this board:
- *       .  .  .
- *       O  O  .     (where . is off, and O is on)
- *       .  .  .
- *
- *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
- *
- *  This should render an HTML table of individual <Cell /> components.
- *
- *  This doesn't handle any clicks --- clicks are on individual cells
- *
- **/
+  function createBoard() {
+    const board = [];
 
-class Board extends Component {
+    for (let i = 0; i < nrows; i++) {
+      const row = [];
 
-  constructor(props) {
-    super(props);
+      for (let k = 0; k < ncols; k++) {
+        row.push(Math.random() < chanceLightStartsOn);
+      }
 
-    // TODO: set initial state
+      board.push(row);
+    }
+
+    return board;
   }
 
-  /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
+  const flipCellsAround = coord => {
+    const [y, x] = coord.split('-').map(Number);
 
-  createBoard() {
-    let board = [];
-    // TODO: create array-of-arrays of true/false values
-    return board
-  }
-
-  /** handle changing a cell: update board & determine if winner */
-
-  flipCellsAround(coord) {
-    let {ncols, nrows} = this.props;
-    let board = this.state.board;
-    let [y, x] = coord.split("-").map(Number);
-
-
-    function flipCell(y, x) {
-      // if this coord is actually on board, flip it
-
+    const flipCell = (y, x) => {
       if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
         board[y][x] = !board[y][x];
       }
-    }
+    };
 
-    // TODO: flip this cell and the cells around it
+    flipCell(y, x);
+    flipCell(y, x - 1);
+    flipCell(y, x + 1);
+    flipCell(y - 1, x);
+    flipCell(y + 1, x);
 
-    // win when every cell is turned off
-    // TODO: determine is the game has been won
+    const isWin = board.every(row => row.every(cell => !cell));
 
-    this.setState({board, hasWon});
-  }
+    setHasWon(isWin);
+    setBoard([...board]);
+  };
 
+  const returnCells = () => {
+    return board.map((row, rIdx) => {
+      return (
+        <tr key={rIdx}>
+          {row.map((col, cIdx) => {
+            const coord = `${rIdx}-${cIdx}`;
+            return <Cell isLit={col} key={coord} flipCellsAroundMe={() => flipCellsAround(coord)} />;
+          })}
+        </tr>
+      );
+    });
+  };
 
-  /** Render game board or winning message. */
+  if (hasWon)
+    return (
+      <div className="Board-title">
+        <div className="neon">You</div>
+        <div className="flux">Win</div>
+      </div>
+    );
 
-  render() {
-
-    // if the game is won, just show a winning msg & render nothing else
-
-    // TODO
-
-    // make table board
-
-    // TODO
-  }
+  return (
+    <div>
+      <div className="Board-title">
+        <div className="neon">Lights</div>
+        <div className="flux">Out</div>
+      </div>
+      <table className="Board">
+        <tbody>{returnCells()}</tbody>
+      </table>
+    </div>
+  );
 }
-
-
-export default Board;
--- src/Cell.js
+++ src/Cell.js
@@ -1,39 +1,11 @@
-import React, {Component} from 'react'
-import "./Cell.css"
+import './Cell.css';
 
+export default function Cell({ flipCellsAroundMe, isLit }) {
+  const handleClick = evt => {
+    flipCellsAroundMe();
+  };
 
-/** A single cell on the board.
- *
- * This has no state --- just two props:
- *
- * - flipCellsAroundMe: a function rec'd from the board which flips this
- *      cell and the cells around of it
- *
- * - isLit: boolean, is this cell lit?
- *
- * This handles clicks --- by calling flipCellsAroundMe
- *
- **/
+  let classes = 'Cell' + (isLit ? ' Cell-lit' : '');
 
-class Cell extends Component {
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(evt) {
-    // call up to the board to flip cells around this cell
-    this.props.flipCellsAroundMe();
-  }
-
-  render() {
-    let classes = "Cell" + (this.props.isLit ? " Cell-lit" : "");
-
-    return (
-        <td className={classes} onClick={this.handleClick} />
-    )
-  }
+  return <td className={classes} onClick={handleClick} />;
 }
-
-
-export default Cell
